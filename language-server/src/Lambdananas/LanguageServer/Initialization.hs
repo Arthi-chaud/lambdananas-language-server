e6d3cba225e54b6c9a5ee60ca429d7dae300d918
Arthur Jamet <arthur.jamet@gmail.com>
2024-11-09 14:41:23 +0000

    Language Server: Remove Progress notification on initialisation, as it is not allowed in specs

--- language-server/src/Lambdananas/LanguageServer/Initialization.hs
+++ language-server/src/Lambdananas/LanguageServer/Initialization.hs
@@ -7,17 +7,13 @@ import Lambdananas.LanguageServer.Logging (debugLog, errorLog)
 import Lambdananas.LanguageServer.Messages (sendErrorMessage)
 import Lambdananas.LanguageServer.Monad
 import Lambdananas.Wrapper
-import Language.LSP.Server
 
 initialize :: LSM ()
-initialize = withIndefiniteProgress
-    "Analysing Coding Style"
-    NotCancellable
-    $ do
-        exists <- liftIO lambdananasExists
-        if exists
-            then debugLog "Lambdanas was found!"
-            else do
-                errorLog "Lambdananas was not found"
-                sendErrorMessage
-                    "Lambdananas was not found in PATH. Did you install it?"
+initialize = do
+    exists <- liftIO lambdananasExists
+    if exists
+        then debugLog "Lambdanas was found!"
+        else do
+            errorLog "Lambdananas was not found"
+            sendErrorMessage
+                "Lambdananas was not found in PATH. Did you install it?"
