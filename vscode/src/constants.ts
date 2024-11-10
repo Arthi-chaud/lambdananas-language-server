9101b73965247d016a8f173f20132ee28faa1989
Arthur Jamet <arthur.jamet@gmail.com>
2024-11-10 16:38:41 +0000

    VSCode Plugin: Init + Launch Lambdananas

---
+++ vscode/src/constants.ts
@@ -0,0 +1,8 @@
+export const RestartServerCommandName = "lambdananas.commands.restartServer";
+export const StartServerCommandName = "lambdananas.commands.restartServer";
+export const StopServerCommandName = "lambdananas.commands.stopServer";
+export const OpenCodingStyleDocCommandName =
+  "lambdananas.commands.openCodingStylePDF";
+
+export const CodingStylePDFUrl =
+  "https://intra.epitech.eu/file/Public/technical-documentations/Haskell/epitech_haskell_coding_style.pdf";
