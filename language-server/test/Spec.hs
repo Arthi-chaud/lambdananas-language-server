91f5e318b93f783097f849b75bfa75aa47f78293
Arthur Jamet <arthur.jamet@gmail.com>
2024-11-12 09:55:55 +0000

    Language Server: Test Code Action

--- language-server/test/Spec.hs
+++ language-server/test/Spec.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import qualified LambdananasTest.LanguageServer.CodeActions as CodeActions
 import qualified LambdananasTest.LanguageServer.Diagnostics as Diagnostics
 import qualified LambdananasTest.Wrapper.Warn as Warn
 import Test.Hspec
@@ -9,3 +10,4 @@ main = hspec $ do
     describe "Wrapper" Warn.specs
     describe "LanguageServer" $ do
         Diagnostics.specs
+        CodeActions.specs
