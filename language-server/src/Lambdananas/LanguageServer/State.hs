184f53b266ff730143fceac624b4d74d890c50d5
Arthur Jamet <arthur.jamet@gmail.com>
2024-11-12 16:58:40 +0000

    Language Server: Handle File Events

--- language-server/src/Lambdananas/LanguageServer/State.hs
+++ language-server/src/Lambdananas/LanguageServer/State.hs
@@ -1,6 +1,24 @@
-module Lambdananas.LanguageServer.State (State) where
+module Lambdananas.LanguageServer.State (State, removeDiagnsotics, moveDiagnostics) where
 
+import Data.Maybe (fromMaybe)
 import Lambdananas.Wrapper.Warn
+import Language.LSP.Protocol.Types
 
 -- | Note: We expect 'FilePath' to be absolute
 type State = [(FilePath, [CodingStyleWarning])]
+
+removeDiagnsotics :: Uri -> State -> State
+removeDiagnsotics uri state = case uriToFilePath uri of
+    Nothing -> state
+    Just fp -> filter (\(key, _) -> key /= fp) state
+
+moveDiagnostics :: Uri -> Uri -> State -> State
+moveDiagnostics fromUri toUri state = fromMaybe state $ do
+    fromFilePath <- uriToFilePath fromUri
+    toFilePath <- uriToFilePath toUri
+    return $ replace fromFilePath toFilePath state
+  where
+    replace from to (a : b)
+        | fst a == from = (to, snd a) : b
+        | otherwise = a : replace from to b
+    replace _ _ [] = []
